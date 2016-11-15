bff00caf1f6db73afe8364617a1ff356d8d2e2df
Andy Hawkins — (̖̣͕̠̬̭̞̪̐̅ỉ̭̣͈̪̠ͮs̳̳̦̞̰̜̞̳͑͌͋ͪ̃̍ ̩̘́͑ͨ̈́̎̋ͮ͊̀ñ͔̣͓̬̈ͮͦ́ͫ͐͛ͅͅo̩̦̳̠̙̗̯̺̐͆̽t̜̥̻̙̟̼̜͂ͩ͋̅̈́ ̠̦̹̟͕͕̱͉͒͋̅́͐ā̟̻̱ͦ̒̍͋ ̗̞͚ͨͩ͊͂̉ͅh͔̼͚̩͚̪̝̝ͣa̫̫̒̓ͬc̦̲̳͍̹̃ͥͯk̲͕̗̖̤̙̜͍̊e̫͎̟̼̺̫̿r͙̰̊̑̿̅ͯͅ)̪̖͇̝̮̪͖̦͆̇ͧ͑̚ <a904guy@users.noreply.github.com>
2016-11-15 14:47:03 -0500

    Update itertools_product_bigData.py

--- src/itertools_product_bigData.py
+++ src/itertools_product_bigData.py
@@ -2,7 +2,7 @@ def total_product(*args):
 
     """Calculate total permutations of a Cartesian product
 
-    :args args is the iterates that will eventually be passed to more_itertools.specific_product()
+    :args args is the iterates that would of been passed to more_itertools.specific_product()
 
     Used to get an integer of for iteration over with specific Cartesian product
     """
@@ -19,7 +19,7 @@ def specific_product(sets, n):
 
     """Extract single permutation of Cartesian product iteration in an consistent speed regardless of list/set sizes.
 
-    :arg sets: The same arg used for itertools.product, iterates.
+    :arg sets: The same arg of sets used for itertools.product, iterates.
     :arg n: Integer of the specific permutation of the product you want to extract.
 
     This is better then the current itertools generator/yield when dealing with massive amounts of sets/permutations.
